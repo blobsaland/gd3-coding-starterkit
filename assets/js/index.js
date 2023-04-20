17797e5867cccd97fff15a09a12501622d88b950
blobsaland <dokterpeper3000@gmail.com>
2023-04-20 14:07:26 +0200

    shit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,39 @@
 const nameInput = document.getElementById("nameInput");
 const artistNameInput = document.getElementById("artistNameInput");
+
+const sumbmitButton = document.getElementById("sumbmitButton");
+const result = document.getElementById("result");
+
 let portetData =
   {
     nameInput : "",
     artistNameInput : "",
   };
 
-nameInput.addEventListener("change", function() {
-  // console.log(nameInput.value)
-  portetData.nameInput = nameInput.value
-  console.log(portetData)
+// nameInput.addEventListener("change", function() {
+//   // console.log(nameInput.value)
+//   portetData.nameInput = nameInput.value
+//   console.log(portetData)
 
 
-});
+// });
+// artistNameInput.addEventListener("change", function() {
+//   // console.log(artistNameInput.value)
+//   portetData.artistNameInput = artistNameInput.value
+//   console.log(portetData)
+// });
 
-artistNameInput.addEventListener("change", function() {
-  // console.log(artistNameInput.value)
-  portetData.artistNameInput = artistNameInput.value
-  console.log(portetData)
+sumbmitButton.addEventListener("click", function() {
+  var allInputs = document.getElementsByTagName('input');
+  console.log(allInputs)
 
-
-});
-
-
-artistNameInput.addEventListener("change", function() {
-  var ele = document.getElementsByName('gender');
-    
-  for(i = 0; i < ele.length; i++) {
-      if(ele[i].checked)
-      document.getElementById("result").innerHTML
-              = "Gender: "+ele[i].value;
+  for(let i = 0; i < allInputs.length; i++) {
+        
+      if(allInputs[i].type="radio") {
+        
+          if(allInputs[i].checked){
+            console.log(allInputs[i].name + allInputs[i].value) 
+          }       
+      }
   }
-}
+});
