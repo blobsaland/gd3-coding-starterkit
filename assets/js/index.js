28a424b290b0865e00fc66df013cc65f96c7907e
blobsaland <dokterpeper3000@gmail.com>
2023-04-20 10:53:24 +0200

    test 1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,34 @@
-const helloWorld = () => {
-  console.log("Hello World");
-};
+const nameInput = document.getElementById("nameInput");
+const artistNameInput = document.getElementById("artistNameInput");
+let portetData =
+  {
+    nameInput : "",
+    artistNameInput : "",
+  };
 
-helloWorld();
+nameInput.addEventListener("change", function() {
+  // console.log(nameInput.value)
+  portetData.nameInput = nameInput.value
+  console.log(portetData)
+
+
+});
+
+artistNameInput.addEventListener("change", function() {
+  // console.log(artistNameInput.value)
+  portetData.artistNameInput = artistNameInput.value
+  console.log(portetData)
+
+
+});
+
+
+artistNameInput.addEventListener("change", function() {
+  var ele = document.getElementsByName('gender');
+    
+  for(i = 0; i < ele.length; i++) {
+      if(ele[i].checked)
+      document.getElementById("result").innerHTML
+              = "Gender: "+ele[i].value;
+  }
+}
