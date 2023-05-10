ddb6a48339015ac0eb39a6b220eac89e5cd339ad
blobsaland <dokterpeper3000@gmail.com>
2023-05-10 13:19:07 +0200

    make the form work

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,39 +1,10 @@
-const nameInput = document.getElementById("nameInput");
-const artistNameInput = document.getElementById("artistNameInput");
+const form = document.getElementById('info');
 
-const sumbmitButton = document.getElementById("sumbmitButton");
-const result = document.getElementById("result");
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const myFormData = new FormData(event.target);
 
-let portetData =
-  {
-    nameInput : "",
-    artistNameInput : "",
-  };
-
-// nameInput.addEventListener("change", function() {
-//   // console.log(nameInput.value)
-//   portetData.nameInput = nameInput.value
-//   console.log(portetData)
-
-
-// });
-// artistNameInput.addEventListener("change", function() {
-//   // console.log(artistNameInput.value)
-//   portetData.artistNameInput = artistNameInput.value
-//   console.log(portetData)
-// });
-
-sumbmitButton.addEventListener("click", function() {
-  var allInputs = document.getElementsByTagName('input');
-  console.log(allInputs)
-
-  for(let i = 0; i < allInputs.length; i++) {
-        
-      if(allInputs[i].type="radio") {
-        
-          if(allInputs[i].checked){
-            console.log(allInputs[i].name + allInputs[i].value) 
-          }       
-      }
-  }
+  const formDataObj = {};
+    myFormData.forEach((value, key) => (formDataObj[key] = value));
+    console.log(formDataObj);
 });
