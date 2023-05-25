1a7a2744670aacafeec349dc8ad03275cf27d2fe
blobsaland <dokterpeper3000@gmail.com>
2023-05-25 13:03:52 +0200

    Add images

---
+++ assets/js/sketch.js
@@ -0,0 +1,62 @@
+
+
+// let options = { video: { facingMode: { exact: "environment" } } }
+
+// function preload() {
+
+//  serious = loadImage("assets/images/building imgs/basic.jpg");
+
+// }
+
+// let x1 = 0 
+// let x2= 0
+// let y1 = 0
+// let y2= 0
+   
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//   drawLayer = createGraphics(windowWidth, windowHeight);
+//   prevLayer = createGraphics(windowWidth, windowHeight);
+//   menuLayer = createGraphics(windowWidth, windowHeight);
+
+//   // capture = createCapture(VIDEO);
+//     background(220)
+
+// }
+
+// function draw() {
+  
+//   // image(capture, 0, 0, width, height, 0, 0, capture.width, capture.height, COVER, CENTER);
+//   // capture.hide();
+  
+//   background(0)
+//   image(drawLayer, 0, 0);
+//   image(prevLayer, 0, 0);
+  
+//   image(menuLayer, 0, 0);
+
+// }
+
+// function touchStarted(event){
+// x1 = mouseX
+// y1 = mouseY
+// }
+
+// function   touchEnded(event) {
+// x2 = mouseX
+// y2 = mouseY
+// drawLayer.image(serious, x1, y1, x2-x1, y2-y1);
+// }
+
+// function touchMoved() {
+//   prevLayer.clear()
+//  x2 = mouseX
+// y2 = mouseY
+// console.log(x2,y2)
+//   if(!(x1-x2 === 0 || y1-y2 === 0)){
+//     prevLayer.image(serious, x1, y1, x2-x1, y2-y1); 
+
+//   }
+// }
+  
+
